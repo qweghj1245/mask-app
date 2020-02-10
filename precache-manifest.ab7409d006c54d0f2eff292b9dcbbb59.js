26da22fe9a01066a32437bb010a35befda72cb93
mazusu <qweghj1245@gmail.com>
2020-02-11 00:26:13 +0800

    Updates

---
+++ precache-manifest.ab7409d006c54d0f2eff292b9dcbbb59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b587fe5538b4bff3356a6eeedbe688",
+    "url": "/qweghj1245/mask-app/index.html"
+  },
+  {
+    "revision": "4d247017bf6a698aeec8",
+    "url": "/qweghj1245/mask-app/static/css/2.103ba269.chunk.css"
+  },
+  {
+    "revision": "1ae9de576e5321a4572d",
+    "url": "/qweghj1245/mask-app/static/css/main.fd84a144.chunk.css"
+  },
+  {
+    "revision": "4d247017bf6a698aeec8",
+    "url": "/qweghj1245/mask-app/static/js/2.2cb1a79c.chunk.js"
+  },
+  {
+    "revision": "bd1838d203d1979a02c0dec1831b19fc",
+    "url": "/qweghj1245/mask-app/static/js/2.2cb1a79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae9de576e5321a4572d",
+    "url": "/qweghj1245/mask-app/static/js/main.90e13e11.chunk.js"
+  },
+  {
+    "revision": "3524651660ca8183a089",
+    "url": "/qweghj1245/mask-app/static/js/runtime-main.05071f7d.js"
+  },
+  {
+    "revision": "b031773073b2c659f0892d66786d26c9",
+    "url": "/qweghj1245/mask-app/static/media/Icon_location_green.b0317730.svg"
+  },
+  {
+    "revision": "359a86cc10b9611bfcdf6307b68c7db9",
+    "url": "/qweghj1245/mask-app/static/media/Icon_location_orange.359a86cc.svg"
+  },
+  {
+    "revision": "abc33708c4dcfa56722c59e01c9cc366",
+    "url": "/qweghj1245/mask-app/static/media/Icon_location_red.abc33708.svg"
+  },
+  {
+    "revision": "7bf22cebe2555e6993e0feb300ce8499",
+    "url": "/qweghj1245/mask-app/static/media/Icon_search.7bf22ceb.svg"
+  },
+  {
+    "revision": "0a9c376b6897f0e3d7d1651cb402be90",
+    "url": "/qweghj1245/mask-app/static/media/img_bg_orange.0a9c376b.svg"
+  }
+]);
